87f956cb23d976a9eb4d5e509aaf609f5699a7dd
Monish <monish.vachhani@gmail.com>
2013-04-10 15:30:30 -0400

    Bhattacharyya Added

--- WebSearchEngineHW3/src/edu/nyu/cs/cs2580/Bhattacharyya.java
+++ WebSearchEngineHW3/src/edu/nyu/cs/cs2580/Bhattacharyya.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class QuerySimilarity {
+public class Bhattacharyya {
 	
 	private String directoryPath;
 	File folder = new File(directoryPath);
